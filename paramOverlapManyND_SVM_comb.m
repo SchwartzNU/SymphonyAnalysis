ccb2a4cd9d1bd58a7c986d7e24cfd4538ddd04d8
Greg Schwartz <greg.schwartz@northwestern.edu>
2015-01-15 22:40:18 -0600

    Fixed this function up quite a bit. Among other changes, it now returns the coefficients for the optimal separating line.

--- paramOverlapManyND_SVM_comb.m
+++ paramOverlapManyND_SVM_comb.m
@@ -1,11 +1,15 @@
-function paramOverlapManyND_SVM_comb(analysisTree, paramList, cellTypeList, testAll)
+function [SVMstructs, predictions, param1, param2, paramVals, lineCoeffs, minErrs] = paramOverlapManyND_SVM_comb(analysisTree, paramList, cellTypeList, testAll)
 %paramList: Greg's format is [L1 L2 L3] = getParameterListByType(nodeData);
 %Adam's format is L = adaptParamList(nodeData).
 %Here use Adam's.
 
+kernelType = 'linear';
+
 if nargin<4
     testAll = true;
 end
+
+[~, paramList] = narrowDownLists(analysisTree,cellTypeList, paramList);
 
 N = length(cellTypeList)
 if testAll
@@ -77,7 +81,6 @@ numPlanes = size(C, 1);
 
 
 f1 = figure(1);
-f2 = figure(2);
 for g=1:Ngroups
     oneInd = find(groupIndexList(g,:) == 1);
     zeroInd = find(groupIndexList(g,:) == 0);
@@ -85,6 +88,8 @@ for g=1:Ngroups
     oneGroupTypes = cellTypeList(oneInd);
     zeroGroupTypes = cellTypeList(zeroInd);
     
+    lossVals = NaN*ones(1,numPlanes);
+    numErrs = NaN*ones(1,numPlanes);
     for i=1:numPlanes
         disp([num2str(g) ' of ' num2str(Ngroups) ' groups']);
         disp([num2str(i) ' of ' num2str(numPlanes) ' param pairs']);
@@ -93,53 +98,67 @@ for g=1:Ngroups
         trained = false;
         try
             %figure(f1);
-            SVMstruct_lin = svmtrain(TrainingMAT_part, GroupsMAT_temp, 'kernel_function', 'linear', 'showplot', false);   
+            SVMstruct = svmtrain(TrainingMAT_part, GroupsMAT_temp, 'kernel_function', kernelType, 'showplot', false);
+            %SVMstruct_lin = fitcsvm(TrainingMAT_part, GroupsMAT_temp, 'KernelFunction', kernelType);
             trained = true;
         catch
             disp('training error');
         end
-        if trained
-            Group_classified_lin = svmclassify(SVMstruct_lin,TrainingMAT_part);
-            numErrors_lin = sum(abs(GroupsMAT_temp - Group_classified_lin)) %anything not 0 is an error
-            if numErrors_lin==0
-                figure(f1);
-                SVMstruct_lin = svmtrain(TrainingMAT_part, GroupsMAT_temp, 'kernel_function', 'linear', 'showplot', true);
-                xlabel(paramList{C(i,1)}, 'Interpreter', 'none');
-                ylabel(paramList{C(i,2)}, 'Interpreter', 'none');
-                title(['Lin: 0: ' zeroGroupTypes' ' 1: ' oneGroupTypes']);
-                pause;
-            else
-                clf(f1)
-            end
+        if trained          
+            prediction = svmclassify(SVMstruct, TrainingMAT_part);
+            numErrs(i) = sum(abs(GroupsMAT_temp - prediction));
         end
 %         %quad
-        trained = false;
-        try
-            %figure(f2);
-            SVMstruct_quad = svmtrain(TrainingMAT_part, GroupsMAT_temp, 'kernel_function', 'quadratic', 'showplot', false);   
-            trained = true;
-        catch
-            disp('training error');
-        end
-        if trained
-            Group_classified_quad = svmclassify(SVMstruct_quad,TrainingMAT_part);
-            numErrors_quad = sum(abs(GroupsMAT_temp - Group_classified_quad)) %anything not 0 is an error
-            if numErrors_quad==0   
-                figure(f2);
-                SVMstruct_quad = svmtrain(TrainingMAT_part, GroupsMAT_temp, 'kernel_function', 'quadratic', 'showplot', true);
-                xlabel(paramList{C(i,1)}, 'Interpreter', 'none');
-                ylabel(paramList{C(i,2)}, 'Interpreter', 'none');
-                title(['Quad: 0: ' zeroGroupTypes' ' 1: ' oneGroupTypes']);
-                pause;
-            else
-                clf(f2)
-            end
-        end
+%        trained = false;
+%         try
+%             %figure(f2);
+%             SVMstruct_quad = svmtrain(TrainingMAT_part, GroupsMAT_temp, 'kernel_function', 'quadratic', 'showplot', false);   
+%             trained = true;
+%         catch
+%             disp('training error');
+%         end
+%         if trained
+%             Group_classified_quad = svmclassify(SVMstruct_quad,TrainingMAT_part);
+%             numErrors_quad = sum(abs(GroupsMAT_temp - Group_classified_quad)) %anything not 0 is an error
+%             if numErrors_quad<errorThres  
+%                 figure(f2);
+%                 SVMstruct_quad = svmtrain(TrainingMAT_part, GroupsMAT_temp, 'kernel_function', 'quadratic', 'showplot', true);
+%                 xlabel(paramList{C(i,1)}, 'Interpreter', 'none');
+%                 ylabel(paramList{C(i,2)}, 'Interpreter', 'none');
+%                 title(['Quad: 0: ' zeroGroupTypes' ' 1: ' oneGroupTypes']);
+%                 pause;
+%             else
+%                 clf(f2)
+%             end
+%         end
+    end   
+    [minErrs, minInd] = min(numErrs)
+    for i=1:length(minInd)        
+        curInd = minInd(i);
+        TrainingMAT_part = TrainingMAT(:, C(curInd,:));
+        %for plotting
+        figure(1);
+        SVMstruct = svmtrain(TrainingMAT_part, GroupsMAT_temp, 'kernel_function', kernelType, 'showplot', true);
+        SVMstructs(i) = SVMstruct;
+
+        w1 = dot(SVMstruct.Alpha, SVMstruct.SupportVectors(:,1));
+        w2 = dot(SVMstruct.Alpha, SVMstruct.SupportVectors(:,2));        
+        % with line given as y = a*x + b
+        a = -w1/w2;
+        b = -SVMstruct.Bias/w2;        
+        a_scaled = a * SVMstructs.ScaleData.scaleFactor(1) / SVMstructs.ScaleData.scaleFactor(2);
+        b_scaled = b * SVMstructs.ScaleData.scaleFactor(2) + SVMstructs.ScaleData.shift(2);
+        lineCoeffs(i,:) = [a_scaled, b_scaled]; %equation of the separating line        
+        
+        xlabel(paramList{C(curInd,1)}, 'Interpreter', 'none');
+        ylabel(paramList{C(curInd,2)}, 'Interpreter', 'none');
+        title([kernelType ' 0: ' zeroGroupTypes' ' 1: ' oneGroupTypes']);       
+        param1{i} = paramList{C(curInd,1)};
+        param2{i} = paramList{C(curInd,2)};
+        paramVals{i} = TrainingMAT_part;
+        predictions{i} = svmclassify(SVMstructs, TrainingMAT_part);
+        pause;
     end
-    
 end
-
-
-
 
 
