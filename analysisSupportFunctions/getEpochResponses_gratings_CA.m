4a6317b0bc0c1687640f1c3f3b573bc212aff554
Greg Schwartz <greg.schwartz@northwestern.edu>
2015-06-04 13:35:50 -0500

    gratings analysis

---
+++ analysisSupportFunctions/getEpochResponses_gratings_CA.m
@@ -0,0 +1,142 @@
+function outputStruct = getEpochResponses_gratings_CA(cellData, epochInd, varargin)
+ip = inputParser;
+ip.KeepUnmatched = true;
+ip.addParamValue('DeviceName', 'Amplifier_Ch1', @(x)ischar(x));
+ip.addParamValue('BaselineTime', 0, @(x)isnumeric(x)); %ms - overrides preTime for baseline interval calculation, use if preTime is missing
+ip.addParamValue('StartTime', 0, @(x)isnumeric(x)); %ms with 0 as stimulus start time
+ip.addParamValue('EndTime', 0, @(x)isnumeric(x)); %ms
+ip.addParamValue('LowPassFreq', 30, @(x)isnumeric(x)); %Hz
+ip.addParamValue('BinWidth', 10, @(x)isnumeric(x)); %ms
+ip.addParamValue('EndOffset', 0, @(x)isnumeric(x)); %ms
+
+ip.parse(varargin{:});
+
+L = length(epochInd);
+
+sampleEpoch = cellData.epochs(epochInd(1));
+
+intervalStart = ip.Results.StartTime * 1E-3; %s
+if ip.Results.EndTime == 0 %default to end at stimEnd
+    intervalEnd = (sampleEpoch.get('stimTime') + ip.Results.EndOffset) * 1E-3; %s
+else
+    intervalEnd = ip.Results.EndTime * 1E-3; %s
+end
+
+%first pass through epochsis for getting baseline spikes, ISIs.
+%These will be used later for figuring out the response probability
+
+blISI = cell(1,L); %initialize ISI dist
+blISI2 = cell(1,L);
+fullISI = [];
+fullSpikeAmpDiff = [];
+for i=1:L
+    curEpoch = cellData.epochs(epochInd(i));
+    
+    if i==1 %some stuff we only need to do once
+        sampleRate = curEpoch.get('sampleRate');
+        [~, xvals] = curEpoch.getData(ip.Results.DeviceName);
+        [~, stimStart] = min(abs(xvals)); %closest point to zero
+        stimStart = stimStart(1);
+        %set up intervals for spike counting
+        baselineStart = xvals(1);
+        baselineEnd = ip.Results.BaselineTime * 1E-3;
+        %         responseIntverval = xvals >= intervalStart & xvals < intervalEnd;
+        %         baselineInterval = xvals < ip.Results.BaselineTime * 1E-3;
+        responseIntervalLen = intervalEnd - intervalStart; %s
+        baselineIntervalLen = baselineEnd - baselineStart; %s
+    end
+    
+    %get spike times (in units of seconds from startTime
+    spikeTimes = curEpoch.getSpikes(ip.Results.DeviceName);
+    spikeTimes = spikeTimes - stimStart;
+    spikeTimes = spikeTimes / sampleRate;
+    
+    %TEMP HACK: This is to remove crazy refractory preiod violations from the spike detector
+    if  length(spikeTimes) >= 2
+        ISItest = diff(spikeTimes);
+        spikeTimes = spikeTimes([(ISItest > 0.0015) true]);
+    end;
+    
+    %get baseline spikes
+    baselineSpikeTimes = spikeTimes(spikeTimes < baselineEnd);
+    outputStruct.baselineRate.value(i) = length(baselineSpikeTimes) / baselineIntervalLen;
+end
+
+
+%get meanBaseline: to be used throughout
+meanBaselineRate = mean(outputStruct.baselineRate.value);
+
+%initalize a few things
+ONSETresponseStartTime_all = ones(1,L) * NaN;
+ONSETresponseEndTime_all = ones(1,L) * NaN;
+OFFSETresponseStartTime_all = ones(1,L) * NaN;
+OFFSETresponseEndTime_all = ones(1,L) * NaN;
+
+%loop over epochs to calculate responses
+for i=1:L
+    %TODO: deal with missing "value" fields as NaN throughout (or fill them
+    %in as such at the end)
+    if i==1 %some stuff we only need to do once: units and types for each output
+        
+        outputStruct.baselineRate.units = 'Hz';
+        outputStruct.baselineRate.type = 'byEpoch';
+        %value already set
+
+        outputStruct.cycleAvgPSTH_x.units = 's';
+        outputStruct.cycleAvgPSTH_x.type = 'combinedAcrossEpochs';
+        outputStruct.cycleAvgPSTH_x.value = [];     
+        
+        outputStruct.cycleAvgPSTH_y.units = 'Hz';
+        outputStruct.cycleAvgPSTH_y.type = 'combinedAcrossEpochs';
+        outputStruct.cycleAvgPSTH_y.value = []; 
+        
+        outputStruct.F1amplitude.units = 'Hz/s^2'; %?
+        outputStruct.F1amplitude.type = 'singleValue';
+        outputStruct.F1amplitude.value = NaN;
+        
+        outputStruct.F2amplitude.units = 'Hz/s^2'; %?
+        outputStruct.F2amplitude.type = 'singleValue';
+        outputStruct.F2amplitude.value = NaN;
+        
+        outputStruct.F2overF1.units = '';
+        outputStruct.F2overF1.type = 'singleValue';
+        outputStruct.F2overF1.value = NaN;
+        
+    end
+    
+    
+    
+end
+
+[psth, xvals] = cellData.getPSTH(epochInd, ip.Results.BinWidth, ip.Results.DeviceName);
+sampleEpoch = cellData.epochs(epochInd(1));
+sampleRate = sampleEpoch.get('sampleRate');
+binWidth = 10; %ms
+%get bins
+samplesPerMS = sampleRate/1E3;
+samplesPerBin = binWidth*samplesPerMS;
+
+freq = sampleEpoch.get('temporalFreq'); %Hz
+cyclePts = floor(sampleRate/samplesPerBin/freq);
+numCycles = floor(length(psth) / cyclePts);
+
+% Get the average cycle.
+cycles = zeros(numCycles, cyclePts);
+for j = 1 : numCycles
+    index = round(((j-1)*cyclePts + (1 : floor(cyclePts))));
+    cycles(j,:) =  psth(index);
+end
+% Take the mean, skipping first cycle
+avgCycle = mean(cycles(2:end),1);
+outputStruct.cycleAvgPSTH_y.value = avgCycle;
+outputStruct.cycleAvgPSTH_x.value = xvals(1:length(avgCycle));
+
+% Do the FFT.
+ft = fft(avgCycle);
+
+% Pull out the F1 and F2 amplitudes.
+outputStruct.F1amplitude.value = abs(ft(2))/length(ft)*2;
+outputStruct.F2amplitude.value = abs(ft(3))/length(ft)*2;
+outputStruct.F2overF1.value = abs(ft(3))/abs(ft(2));
+
+
---
+++ analysisSupportFunctions/getEpochResponses_gratings_WC.m
@@ -0,0 +1,136 @@
+function outputStruct = getEpochResponses_gratings_WC(cellData, epochInd, varargin)
+ip = inputParser;
+ip.KeepUnmatched = true;
+ip.addParamValue('DeviceName', 'Amplifier_Ch1', @(x)ischar(x));
+ip.addParamValue('BaselineTime', 0, @(x)isnumeric(x)); %ms - overrides preTime for baseline interval calculation, use if preTime is missing
+ip.addParamValue('StartTime', 0, @(x)isnumeric(x)); %ms with 0 as stimulus start time
+ip.addParamValue('EndTime', 0, @(x)isnumeric(x)); %ms
+ip.addParamValue('LowPassFreq', 30, @(x)isnumeric(x)); %Hz
+ip.addParamValue('BinWidth', 10, @(x)isnumeric(x)); %ms
+ip.addParamValue('EndOffset', 0, @(x)isnumeric(x)); %ms
+
+ip.parse(varargin{:});
+
+L = length(epochInd);
+
+
+sampleEpoch = cellData.epochs(epochInd(1));
+if strcmp(ip.Results.DeviceName, 'Amplifier_Ch1')
+    recordingMode = sampleEpoch.get('ampMode');
+elseif strcmp(ip.Results.DeviceName, 'Amplifier_Ch2')
+    recordingMode = sampleEpoch.get('amp2Mode');
+else
+    disp(['Error: unrecognized device ' ip.Results.DeviceName]);
+    return
+end
+
+intervalStart = ip.Results.StartTime * 1E-3; %s
+if ip.Results.EndTime == 0 %default to end at stimEnd
+    intervalEnd = (sampleEpoch.get('stimTime') + ip.Results.EndOffset) * 1E-3; %s
+else
+    intervalEnd = ip.Results.EndTime * 1E-3; %s
+end
+
+sampleRate = sampleEpoch.get('sampleRate');
+[data, xvals] = sampleEpoch.getData(ip.Results.DeviceName);
+%set up intervals
+baselineStart = xvals(1);
+baselineEnd = ip.Results.BaselineTime * 1E-3;
+
+responseIntverval = xvals >= intervalStart & xvals < intervalEnd;
+baselineInterval = xvals < ip.Results.BaselineTime * 1E-3;
+postInterval = xvals >= intervalEnd;
+responseIntervalLen = intervalEnd - intervalStart; %s
+baselineIntervalLen = baselineEnd - baselineStart; %s
+postIntervalLen = xvals(end) - intervalEnd; %s
+
+Mstim = zeros(L, sum(responseIntverval)); %full data matrix, baseline subrtacted on each epoch
+Mpost = zeros(L, sum(postInterval)); %full data matrix, baseline subrtacted on each epoch
+MstimToEnd = zeros(L, sum(responseIntverval) + sum(postInterval));
+baselineVal = zeros(1,L);
+
+for i=1:L
+    curEpoch = cellData.epochs(epochInd(i));
+    %get data
+    [data, xvals, units] = curEpoch.getData(ip.Results.DeviceName);    
+
+    baselineData = data(baselineInterval);
+    baselineVal(i) = mean(baselineData);
+    
+    data = LowPassFilter(data, ip.Results.LowPassFreq, 1/sampleRate);
+    %baseline subtraction
+    data = data - baselineVal(i);
+    stimData = data(responseIntverval);
+    postData = data(postInterval);    
+ 
+    Mstim(i,:) = stimData;   
+    Mstim_baselineSubtracted(i,:) = stimData - baselineVal(i);
+    Mpost(i,:) = postData;
+    stimToEndData = [stimData postData];
+    MstimToEnd(i,:) = stimToEndData;
+    
+end
+
+%loop over epochs to calculate responses
+for i=1:L
+    %TODO: deal with missing "value" fields as NaN throughout (or fill them
+    %in as such at the end)
+    if i==1 %some stuff we only need to do once: units and types for each output
+        
+        outputStruct.baseline.units = units;
+        outputStruct.baseline.type = 'byEpoch';
+        %value already set
+
+        outputStruct.cycleAvg_x.units = 's';
+        outputStruct.cycleAvg_x.type = 'combinedAcrossEpochs';
+        outputStruct.cycleAvg_x.value = [];     
+        
+        outputStruct.cycleAvg_y.units = units;
+        outputStruct.cycleAvg_y.type = 'combinedAcrossEpochs';
+        outputStruct.cycleAvg_y.value = []; 
+        
+        outputStruct.F1amplitude.units = 'pA/s^2'; %?
+        outputStruct.F1amplitude.type = 'singleValue';
+        outputStruct.F1amplitude.value = NaN;
+        
+        outputStruct.F2amplitude.units = 'pA/s^2'; %?
+        outputStruct.F2amplitude.type = 'singleValue';
+        outputStruct.F2amplitude.value = NaN;
+        
+        outputStruct.F2overF1.units = '';
+        outputStruct.F2overF1.type = 'singleValue';
+        outputStruct.F2overF1.value = NaN;
+        
+    end
+    
+end
+
+%baseline 
+outputStruct.baseline.value = mean(baselineVal);
+
+meanTrace_stim = mean(Mstim_baselineSubtracted, 1);
+xvals = [0:length(meanTrace_stim)] ./ sampleRate;
+
+freq = sampleEpoch.get('temporalFreq'); %Hz
+cyclePts = floor(sampleRate/freq);
+numCycles = floor(length(meanTrace_stim) / cyclePts);
+
+% Get the average cycle.
+cycles = zeros(numCycles, cyclePts);
+for j = 1 : numCycles
+    index = round(((j-1)*cyclePts + (1 : floor(cyclePts))));
+    cycles(j,:) =  meanTrace_stim(index);
+end
+% Take the mean, skipping first cycle
+avgCycle = mean(cycles(2:end,:),1);
+outputStruct.cycleAvg_y.value = avgCycle;
+outputStruct.cycleAvg_x.value = xvals(1:length(avgCycle));
+
+% Do the FFT.
+ft = fft(avgCycle);
+% Pull out the F1 and F2 amplitudes.
+outputStruct.F1amplitude.value = abs(ft(2))/length(ft)*2;
+outputStruct.F2amplitude.value = abs(ft(3))/length(ft)*2;
+outputStruct.F2overF1.value = abs(ft(3))/abs(ft(2));
+
+
