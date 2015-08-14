1d93a21f9c86168c01e239567acea23e889e113b
Greg Schwartz <greg.schwartz@northwestern.edu>
2015-08-14 11:16:14 -0500

    PSTH fitting with raised cosines

---
+++ utilities/raisedCosineSum.m
@@ -0,0 +1,17 @@
+function fitVals = raisedCosineSum(params, N, span)
+for i=1:N
+    w(i) = round(params((i-1)*3 + 1));
+    h(i) = params((i-1)*3 + 2);
+    offset(i) = round(params((i-1)*3 + 3));
+end
+
+fitMatrix = zeros(N, span);
+
+for i=1:N
+    fitMatrix(i,:) = rcos(span, w(i), h(i), offset(i));
+end    
+
+%discourage overlap
+fitMins = min(fitMatrix, [], 1);
+fitVals = sum(fitMatrix, 1);
+fitVals(fitMins>0) = Inf;
---
+++ utilities/rcos.m
@@ -0,0 +1,21 @@
+function y = rcos(span, w, h, offset)
+if offset <= 0 || offset + 2*w > span %out of bounds error
+    y = ones(1,span) * inf;
+   return;
+end
+
+if w < 2 %out of bounds error
+   y = ones(1,span) * inf;
+   return;
+end
+
+if h < 0 %out of bounds error
+   y = ones(1,span) * inf;
+   return;
+end
+
+y = zeros(1,span);
+x = linspace(-pi,pi,2*w);
+c = real(cos(x).^0.5);
+c = c.*h;
+y(offset:offset+(2*w)-1) = c;
