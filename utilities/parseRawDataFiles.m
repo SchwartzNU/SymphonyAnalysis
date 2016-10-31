84af54639ed8cc6e74a2febd851bb1f461cc2ea3
Sam Cooler <scooler@gmail.com>
2016-10-31 16:56:54 -0500

    symphony 2 raw data handling

--- utilities/parseRawDataFiles.m
+++ utilities/parseRawDataFiles.m
@@ -28,13 +28,14 @@ for i=1:length(D_raw)
         end
         if writeOK
             tic;
-            disp(['parsing ' curCellName]);
+            disp(['parsing file' curCellName]);
             fname = [rawDataDir curCellName '.h5'];
 
             if h5readatt(fname, '/', 'version') == 2
+                disp('Parsing cells in Symphony 2 file')
                 cells = symphony2Mapper(fname);
                 arrayfun(@(cellData) save([cellDataDir cellData.savedFileName], 'cellData'), cells);
-                disp(['Elapsed time: ' num2str(toc) ' seconds']);   
+                disp(['Elapsed time: ' num2str(toc) ' seconds']);
             else
                 cellData = CellData(fname);
                 save([cellDataDir curCellName], 'cellData');
