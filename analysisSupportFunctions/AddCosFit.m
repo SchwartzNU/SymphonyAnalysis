9c20200d3166114346d5e2f43b77a74dc550e1b0
adammani <mania907@gmail.com>
2017-03-08 17:37:48 -0600

    angle calculation

--- analysisSupportFunctions/AddCosFit.m
+++ analysisSupportFunctions/AddCosFit.m
@@ -13,20 +13,25 @@ function rootData = AddCosFit(rootData)
             beta(4) = -1 * beta(4);
         end
 
-        if beta(3) < 0
-            beta(3) = beta(3) + 180;
-        elseif beta(3) > 180
-            beta(3) = beta(3) - 180;
-        end
-
-        if beta(5) < 0
-            beta(5) = beta(5) + 180;
-        elseif beta(5) > 180
-            beta(5) = beta(5) - 180;
-        end
-
+%         if beta(3) < 0
+%             beta(3) = beta(3) + 180;
+%         elseif beta(3) > 180
+%             beta(3) = beta(3) - 180;
+%         end
+% 
+%         if beta(5) < 0
+%             beta(5) = beta(5) + 180;
+%         elseif beta(5) > 180
+%             beta(5) = beta(5) - 180;
+%         end
+        beta(3) = mod(beta(3),180);
+        beta(5) = mod(beta(5),90);
 
         rootData.spikeCount_stimInterval_grndBlSubt.beta = beta;
+        %Hack Adam 3/6/17
+        for J = 1:5
+            rootData.spikeCount_stimInterval_grndBlSubt.(['beta',num2str(J)]) = beta(J);
+        end;
     end
 
     function y = TwoCos(beta,x)
