a8b7d1b3d7f22920509436a87df746159d0f328e
adammani <mania907@gmail.com>
2017-03-10 15:43:57 -0600

    AddCosFit - fit quality

--- analysisSupportFunctions/AddCosFit.m
+++ analysisSupportFunctions/AddCosFit.m
@@ -1,8 +1,8 @@
 function rootData = AddCosFit(rootData)
     if strcmp(rootData.ampMode, 'Cell attached')
         beta0 = [33 3 3 3 3];
-        beta = nlinfit(rootData.barAngle, rootData.spikeCount_stimInterval_grndBlSubt.mean_c ,@TwoCos,beta0);
-
+        [beta, R,~,~, MSE] = nlinfit(rootData.barAngle, rootData.spikeCount_stimInterval_grndBlSubt.mean_c ,@TwoCos,beta0);
+        
         if beta(2) < 0 
             beta(3) = beta(3) + 90;
             beta(2) = -1 * beta(2);
@@ -28,10 +28,16 @@ function rootData = AddCosFit(rootData)
         beta(5) = mod(beta(5),90);
 
         rootData.spikeCount_stimInterval_grndBlSubt.beta = beta;
+        
         %Hack Adam 3/6/17
         for J = 1:5
             rootData.spikeCount_stimInterval_grndBlSubt.(['beta',num2str(J)]) = beta(J);
         end;
+        %fit quality
+        rootData.spikeCount_stimInterval_grndBlSubt.fitMSE = sqrt(MSE)./beta(1);
+        rootData.spikeCount_stimInterval_grndBlSubt.fitR2 = sqrt(mean(R.^2))./beta(1);
+        rootData.spikeCount_stimInterval_grndBlSubt.fitR1 = mean(abs(R))./beta(1);
+        rootData.spikeCount_stimInterval_grndBlSubt.fitR2abs = sqrt(mean(R.^2));
     end
 
     function y = TwoCos(beta,x)
