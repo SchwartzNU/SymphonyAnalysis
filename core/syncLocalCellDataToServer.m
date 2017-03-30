c1f9763a1e8ebc3b9d6ce644f29e7e4427072192
Sam Cooler <scooler@gmail.com>
2017-03-30 11:52:14 -0500

    sync improvements to use global file locations

--- core/syncLocalCellDataToServer.m
+++ core/syncLocalCellDataToServer.m
@@ -1,36 +1,37 @@
 function [] = syncLocalCellDataToServer()
 global ANALYSIS_FOLDER;
+global CELL_DATA_FOLDER
+global CELL_DATA_MASTER
 
-if exist([filesep 'Volumes' filesep 'SchwartzLab'  filesep 'CellDataMaster']) == 7
-    cellDataMasterFolder = [filesep 'Volumes' filesep 'SchwartzLab'  filesep 'CellDataMaster'];
-else
+if ~(exist(CELL_DATA_MASTER, 'dir') == 7)
     disp('Could not connect to CellDataMaster');
     return;
 end
+
+disp('Sync all local cell data to server')
 
 %get all cellData names in local cellData folder
 cellDataNames = ls([ANALYSIS_FOLDER filesep 'cellData' filesep '*.mat']);
 cellDataNames = strsplit(cellDataNames); %this will be different on windows - see doc ls
 cellDataNames = sort(cellDataNames);
 
-cellDataBaseNames = cell(length(cellDataNames), 1);
+% cellDataBaseNames = cell(length(cellDataNames), 1);
 
 for i=1:length(cellDataNames)
     [~, basename, ~] = fileparts(cellDataNames{i});
     if ~isempty(basename)
-        fileinfo = dir([ANALYSIS_FOLDER 'cellData' filesep basename '.mat']);
+        fileinfo = dir([CELL_DATA_FOLDER basename '.mat']);
         localModDate = fileinfo.datenum;
         try
-            fileinfo = dir([filesep 'Volumes' filesep 'SchwartzLab'  filesep 'CellDataMaster'  filesep basename '.mat']);
+            fileinfo = dir([CELL_DATA_MASTER basename '.mat']);
             serverModDate = fileinfo.datenum;
         catch
             serverModDate = 0;
         end
         if localModDate > serverModDate + 60 %more than 60 seconds newer
            fprintf('Found newer local copy of %s, by %d sec. Copying to server...', basename, round(localModDate - serverModDate));
-           load([ANALYSIS_FOLDER 'cellData' filesep basename '.mat']); %loads cellData
+           load([CELL_DATA_FOLDER basename '.mat']); %loads cellData
            saveAndSyncCellData(cellData);
         end
     end
 end
-
