526fecea4e3e2e6d43a9f90a04975f7d6b065ff8
adammani <mania907@gmail.com>
2017-05-05 12:17:46 -0500

    improved these for TextureMatrix

--- analysisSupportFunctions/addLinearInterp.m
+++ analysisSupportFunctions/addLinearInterp.m
@@ -1,7 +1,7 @@
 function rootData = addLinearInterp(rootData)
 
 if strcmp(rootData.ampMode, 'Cell attached')
-    xvals = rootData.textureHalfMaxScale;
+    xvals = rootData.halfMaxScale;
     yvals = rootData.spikeCount_stimInterval_grndBlSubt.mean_c;
    
     %Column vectors...
@@ -16,20 +16,51 @@ if strcmp(rootData.ampMode, 'Cell attached')
     rootData.fitresult = fitresult;
     rootData.gof = gof;
     
-%     % get the 50% value
-    [~, maxInd] = max(abs(yData));
-    yMax = yData(maxInd);
-    xMax = xData(maxInd);
-    [~, minInd] = min(abs(yData));
-    yMin = yData(minInd);
-    xMin = xData(minInd);
-%     ff = @(x)(feval(fitresult,x)-max(yData)/2);
-%     rootData.crossing = fzero(ff, 20);
-    ff = @(x)(feval(fitresult,x)-max(yData)/2);
-    try
-        rootData.crossing = fzero(ff, [xMin xMax]); 
-    catch
-        rootData.crossing = NaN;
+% %     % get the 50% value
+%     [~, maxInd] = max(abs(yData));
+%     yMax = yData(maxInd);
+%     xMax = xData(maxInd);
+%     [~, minInd] = min(abs(yData));
+%     yMin = yData(minInd);
+%     xMin = xData(minInd);
+% %     ff = @(x)(feval(fitresult,x)-max(yData)/2);
+% %     rootData.crossing = fzero(ff, 20);
+%     ff = @(x)(feval(fitresult,x)-yMax/2);
+%     try
+%         rootData.crossing = fzero(ff, [xMin xMax]);
+%         rootData.crossingY = max(yData)/2;
+%     catch
+%         rootData.crossing = NaN;
+%         rootData.crossingY = NaN;
+%     end;
+
+% %     % get the 50% value
+    rootData.crossing = NaN;
+    rootData.crossingSup = NaN;
+    rootData.crossingY = NaN;
+    rootData.crossingSupY = NaN;
+    [maxY,maxYind] = max(yvals);
+    [minY,minYind]  = min(yvals);
+    if maxY > 0
+        try
+            ff = @(x)(feval(fitresult,x)-maxY/2);
+            rootData.crossing = fzero(ff, xvals(maxYind));
+            rootData.crossingY = maxY/2;
+        catch
+            rootData.crossing = NaN;
+            rootData.crossingY = NaN;
+        end;
+    end;
+    if minY < 0
+        try
+            ff = @(x)(feval(fitresult,x)-minY/2);
+            rootData.crossingSup = fzero(ff, xvals(minYind));
+            rootData.crossingSupY = minY/2;
+        catch
+            rootData.crossingSup = NaN;
+            rootData.crossingSupY = NaN;
+        end;
+        
     end;
 
 end
--- analysisSupportFunctions/addLinearInterpToCR.m
+++ analysisSupportFunctions/addLinearInterpToCR.m
@@ -3,26 +3,28 @@ function rootData = addLinearInterpToCR(rootData)
 if strcmp(rootData.ampMode, 'Cell attached')
     xvals = rootData.contrast;
     yvals = rootData.spikeCount_stimInterval_grndBlSubt.mean_c;
-   
+    
     %Column vectors...
     [xData, yData] = prepareCurveData( xvals, yvals );
-
+    
     % Set up fittype and options.
     ft = 'linearinterp';
-
+    
     % Fit model to data.
     [fitresult, gof] = fit( xData, yData, ft, 'Normalize', 'on' );
     
     rootData.fitresult = fitresult;
     rootData.gof = gof;
     
-%     % get the 50% value
-%     ff = @(x)(feval(fitresult,x)-max(yData)/2);
-%     rootData.crossing = fzero(ff, 20);
+    %     % get the 50% value
+    %     ff = @(x)(feval(fitresult,x)-max(yData)/2);
+    %     rootData.crossing = fzero(ff, 20);
     
     %Positive contrast
     rootData.onCrossing = NaN;
     rootData.onCrossingSup = NaN;
+    rootData.onCrossingY = NaN;
+    rootData.onCrossingSupY = NaN;
     xvalsPos = xvals(xvals>=0);
     yvalsPos = yvals(xvals>=0);
     [maxYpos,maxYposInd] = max(yvalsPos);
@@ -30,15 +32,19 @@ if strcmp(rootData.ampMode, 'Cell attached')
     if maxYpos > 0
         ff = @(x)(feval(fitresult,x)-maxYpos/2);
         rootData.onCrossing = fzero(ff, xvalsPos(maxYposInd));
+        rootData.onCrossingY = maxYpos/2;
     end;
     if minYpos < 0
         ff = @(x)(feval(fitresult,x)-minYpos/2);
         rootData.onCrossingSup = fzero(ff, xvalsPos(minYposInd));
+        rootData.onCrossingSupY = minYpos/2;
     end;
     
     %Negative contrast
     rootData.offCrossing = NaN;
     rootData.offCrossingSup = NaN;
+    rootData.offCrossingY = NaN;
+    rootData.offCrossingSupY = NaN;
     xvalsNeg = xvals(xvals<=0);
     yvalsNeg = yvals(xvals<=0);
     [maxYneg,maxYnegInd] = max(yvalsNeg);
@@ -46,10 +52,12 @@ if strcmp(rootData.ampMode, 'Cell attached')
     if maxYneg > 0
         ff = @(x)(feval(fitresult,x)-maxYneg/2);
         rootData.offCrossing = fzero(ff, xvalsNeg(maxYnegInd));
+        rootData.offCrossingY = maxYneg/2;
     end;
     if minYneg < 0
         ff = @(x)(feval(fitresult,x)-minYneg/2);
         rootData.offCrossingSup = fzero(ff, xvalsNeg(minYnegInd));
+        rootData.offCrossingSupY = minYneg/2;
     end;
     
     
