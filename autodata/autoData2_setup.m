4eece1d60e1ba01bf7efb40c1882a88316e9da87
jilliangoetz <jillian.goetz@northwestern.edu>
2017-06-22 16:03:39 -0500

    Heat map - messing with ticks and boxes

--- autodata/autoData2_setup.m
+++ autodata/autoData2_setup.m
@@ -1,58 +1,24 @@
 global ANALYSIS_FOLDER;
-cellNamesListLocation = [ANALYSIS_FOLDER 'Projects' filesep 'WFDS On and Off and Controls/cellNames.txt'];
+cellNamesListLocation = [ANALYSIS_FOLDER 'Projects' filesep 'rnaseq/cellNames.txt'];
 
 % set this to [] if no external table
-externalTableFilename = 'analysisTrees/automaticData/externalCellDataTable';
+externalTableFilename = [];
 
 % output save location, set to [] to not save
-outputSaveFilename = 'analysisTrees/automaticData/autodata';
+outputSaveFilename = [];
 
 
 warning('off', 'MATLAB:table:RowsAddedExistingVars')
 
 %% load filter/analysis list
-filterFileNames = {'analysisTrees/automaticData/filter light step CA.mat';
-    'analysisTrees/automaticData/filter sms CA.mat';
-    'analysisTrees/automaticData/filter drifting texture CA.mat';
-    'analysisTrees/automaticData/filter drifting gratings CA.mat';
-    'analysisTrees/automaticData/filter sms WC -60.mat';
-    'analysisTrees/automaticData/filter sms WC 20.mat';
-    'analysisTrees/automaticData/filter moving bar 1000 narrow CA.mat';
-    'analysisTrees/automaticData/filter moving bar 500 narrow CA.mat';
-    'analysisTrees/automaticData/filter moving bar 250 narrow CA.mat';
-    'analysisTrees/automaticData/filter light step WC -60.mat';
-    'analysisTrees/automaticData/filter light step WC 20.mat';
-    'analysisTrees/automaticData/filter contrast CA.mat';
-    };
+filterFileNames = {};
 
 % 1 for single params (Light step on spike count mean), 0 for vectors (spike count by spot size), 2 for extracting params for a curve
-treeVariableModes = [1,0,1,1,0,0,1,1,1,2,2,0]; 
+treeVariableModes = []; 
 
-paramsByTree = {{'ONSETspikes_mean', 'OFFSETspikes_mean'};
-    {'ONSETspikes','OFFSETspikes','ONSETrespDuration'};
-    {'spikeCount_stimAfter500ms_mean','spikeCount_stimAfter500ms_DSI', 'spikeCount_stimAfter500ms_DSang','spikeCount_stimAfter500ms_OSI', 'spikeCount_stimAfter500ms_OSang', 'spikeCount_stimAfter500ms_DVar'};
-    {'F1amplitude_mean','F1amplitude_DSI','F1amplitude_DSang','F1amplitude_OSI','F1amplitude_OSang','F1amplitude_DVar'}
-    {'stimInterval_charge','ONSET_peak'};
-    {'stimInterval_charge','ONSET_peak'};
-    {'spikeCount_stimInterval_mean','spikeCount_stimInterval_DSI', 'spikeCount_stimInterval_DSang','spikeCount_stimInterval_OSI', 'spikeCount_stimInterval_OSang', 'spikeCount_stimInterval_DVar'};
-    {'spikeCount_stimInterval_mean','spikeCount_stimInterval_DSI', 'spikeCount_stimInterval_DSang','spikeCount_stimInterval_OSI', 'spikeCount_stimInterval_OSang', 'spikeCount_stimInterval_DVar'};
-    {'spikeCount_stimInterval_mean','spikeCount_stimInterval_DSI', 'spikeCount_stimInterval_DSang','spikeCount_stimInterval_OSI', 'spikeCount_stimInterval_OSang', 'spikeCount_stimInterval_DVar'};
-    {'params'};
-    {'params'};
-    {'ONSETspikes','ONSETlatency'};};
-paramsTypeNames = {'LS_sp','SMS_sp','DrifTex_sp','DrifGrat_sp','SMS_ex','SMS_in','MB1000','MB500','MB250','LS_params_ex','LS_params_in','Contrast_sp'};
-paramsColumnNames = {{'LS_ON_sp','LS_OFF_sp'};
-    {'SMS_spotSize_sp','SMS_onSpikes','SMS_offSpikes','SMS_onDuration'};
-    {'DrifTex_mean_sp','DrifTex_DSI_sp','DrifTex_DSang_sp','DrifTex_OSI_sp','DrifTex_OSang_sp','DrifTex_DVar_sp'};
-    {'DrifGrat_mean_sp','DrifGrat_DSI_sp','DrifGrat_DSang_sp','DrifGrat_OSI_sp','DrifGrat_OSang_sp','DrifGrat_DVar_sp'};
-    {'SMS_spotSize_ex','SMS_charge_ex','SMS_peak_ex'};
-    {'SMS_spotSize_in','SMS_charge_in','SMS_peak_in'};
-    {'MB_1000_mean_sp','MB_1000_DSI_sp','MB_1000_DSang_sp','MB_1000_OSI_sp','MB_1000_OSang_sp','MB_1000_DVar_sp'};
-    {'MB_500_mean_sp','MB_500_DSI_sp','MB_500_DSang_sp','MB_500_OSI_sp','MB_500_OSang_sp','MB_500_DVar_sp'};
-    {'MB_250_mean_sp','MB_250_DSI_sp','MB_250_DSang_sp','MB_250_OSI_sp','MB_250_OSang_sp','MB_250_DVar_sp'};
-    {'LS_ON_params_ex'};
-    {'LS_ON_params_in'};
-    {'Contrast_contrastVal_sp','Contrast_onSpikes','Contrast_onLatency'};};
+paramsByTree = {};
+paramsTypeNames = {};
+paramsColumnNames = {};
 %%
 analyses = table();
 dtabColumns = table();
