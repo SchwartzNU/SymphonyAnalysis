dc5ccbade23a96cf7b90eccbbbeaff7e004880df
jilliangoetz <jillian.goetz@northwestern.edu>
2017-06-22 15:16:10 -0500

    Working heat map generator

--- transcriptomeAnalysys/makeHeatmap.m
+++ transcriptomeAnalysys/makeHeatmap.m
@@ -1,13 +1,27 @@
-function [xtickByCellType] = makeHeatmap(D, cellTypes, geneNames, selection, Ngenes, method)
+function [xtickByCellType] = makeHeatmap(D_full, cellType, uniqueTypes_sorted, NofEach_sorted, allGeneNames, Ngenes)
 
+%Variables
 xtickByCellType = cumsum(NofEach_sorted) - (NofEach_sorted/2) + .5
+cumsum_NofEach_sorted = cumsum(NofEach_sorted)
 
+%figure
 imagesc(log(D_full))
 
-%axis
+%Axis Setup
 ax=gca;
-set(ax,'yTickLabel',allGeneNames)
-set(ax,'ytick', (1:size(D_full,1)))
-set(ax,'xTickLabel',uniqueTypes_sorted)
-set(ax,'xtick', xtickByCellType+.5)
-set(ax,'xTickLabelRotation',45)
+set(ax,'yTickLabel',allGeneNames, 'ytick',(1:size(D_full,1)), 'xTickLabel',uniqueTypes_sorted, 'xtick',xtickByCellType+.5, 'xTickLabelRotation',45);
+
+%Grid Generation
+
+%Y-axis Grid
+for i=1:length(uniqueTypes_sorted)
+    rectangle('Position', [0 (Ngenes*i)+.5 length(cellType)+.5 Ngenes])
+end
+
+%X-axis Grid
+for i=1:(length(uniqueTypes_sorted)-1)
+    rectangle('Position', [cumsum_NofEach_sorted(i)+.5 0 NofEach_sorted(i+1) length(cellType)])
+end
+        
+       
+
