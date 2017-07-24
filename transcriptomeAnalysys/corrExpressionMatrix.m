ddc92c7a3de74a5bc653d55cfdb00fbccf191b07
jilliangoetz <jillian.goetz@northwestern.edu>
2017-07-24 12:38:13 -0500

    corrExpressionMatrix looking much better

--- transcriptomeAnalysys/corrExpressionMatrix.m
+++ transcriptomeAnalysys/corrExpressionMatrix.m
@@ -1,50 +1,52 @@
-function [R, uniqueTypes, corrPValues] = corrExpressionMatrix(D, cellTypes)
-
-%%%% In Progress %%%%
+function [uniqueTypes, corrPValues] = corrExpressionMatrix(D, cellTypes)
 
 uniqueTypes = unique(cellTypes);
 Ntypes = length(uniqueTypes);
 corrPValues = zeros(1,Ntypes);
 
 % Make correlation matrix comparing each cell to every other
-
-R=corrcoef(D)
+R=corrcoef(D);
 
 % Average correlations within cellType
 for i=1:Ntypes
-    currentType = uniqueTypes{i}
-    disp currentType;
+    currentType = uniqueTypes{i};
     
     % Grab indices of all cells of the current type... 
     targetInd = find(strcmp(currentType, cellTypes)); 
-    disp([num2str(length(targetInd)) ' cells of type: ' currentType]);
-    % ... And the indices every other cell too
-    othersInd = setdiff(1:length(cellTypes), targetInd);  
     
-    % This is not the best way to do this as it counts the ones
-    %   on the diagonal. This is probably why everything looks significant
-    %   now, so I'll have to fix that. Just not sure how yet. For loop?
-    
-    %%%  ATTEMPT 1: INCLUDE DIAGONAL 1s. NOT GOOD!
-    % withinTypeCorr = mean(R(targetInd(1:length(targetInd)),targetInd(1:length(targetInd))));
-    %%%
-    
-    %%%  ATTEMPT 2: FOR LOOP TO EXCLUDE 1s
-    withinTypeCorr = zeros(1, length(targetInd));
-    for i=1:length(targetInd);
-        excludeCurrentCell = setdiff(targetInd, i);
-        withinTypeCorr(i) = mean(R(targetInd(i), excludeCurrentCell(1:length(excludeCurrentCell))));
-    end
-    %%%
-    
-    disp(['Average correlation between ' currentType ' cells: ' num2str(withinTypeCorr)]);
+    if(length(targetInd)<3);
+        withinTypeCorr = 0;
+        disp(['Not enough ' currentType ' s to achieve statistical significance.' newline]);
+        corrPValues(i) = NaN;
         
-    betweenTypeCorr = mean(R(targetInd(1:length(targetInd)), othersInd(1:length(othersInd))));
-    disp(['Average correlation between ' currentType ' cells and other cells: ' num2str(betweenTypeCorr)]);
+    else
+        targetIndCombinations = nchoosek(targetInd,2);
+    	disp([num2str(length(targetInd)) ' cells of type: ' currentType]);
     
-    % Run a t-test between the two vectors
-    [~, corrPValues(i)] = ttest2(withinTypeCorr, betweenTypeCorr);
-    disp(['P-Value between ' currentType ' cells and other cells: ' num2str(corrPValues(i))]);
+        % ... And the indices every other cell too
+        othersInd = setdiff(1:length(cellTypes), targetInd);  
+    
+        withinTypeCorr = zeros(1, length(targetIndCombinations));
+    
+        for ci=1:length(targetIndCombinations);
+            withinTypeCorr(ci) = R(targetIndCombinations(ci,1),(targetIndCombinations(ci,2)));
+        end
+        
+        disp(['All correlations between ' currentType ' cells:' num2str(withinTypeCorr)]);
+        withinTypeCorrAvg = mean(withinTypeCorr);
+        disp(['Average correlation between ' currentType ' cells: ' num2str(withinTypeCorrAvg)]);
+        
+        % Between Types    
+        betweenTypeCorr = reshape(R(targetInd(1:length(targetInd)), othersInd(1:length(othersInd))),1,[]);
+        betweenTypeCorrAvg = mean(betweenTypeCorr);
+    
+        disp(['All correlations between ' currentType ' cells and other cells: ' num2str(betweenTypeCorr)]);
+        disp(['Average correlation between ' currentType ' cells: ' num2str(betweenTypeCorrAvg)]);
+    
+        % Run a t-test between the two vectors
+        [~, corrPValues(i)] = ttest2(withinTypeCorr, betweenTypeCorr);
+        disp(['P-Value between ' currentType ' cells and other cells: ' num2str(corrPValues(i)) newline]);
+    end
 end
 
 end
