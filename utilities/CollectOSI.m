8ef7164a21eabf1fa97e41f094770282d31b35cc
davidswygart <davidswygart2015@u.northwestern.edu>
2017-10-16 12:40:34 -0500

    Add plotting utility

--- utilities/CollectOSI.m
+++ utilities/CollectOSI.m
@@ -1,12 +1,9 @@
 function [OSICells, OSI, OSAng] = CollectOSI(analysisClass)
 global ANALYSIS_FOLDER
 
-if nargin == 0
-    [fname, pathname] = uigetfile([ANALYSIS_FOLDER filesep 'analysisTrees' filesep '*.mat'], 'Load analysisTree');
-end
+[fname, pathname] = uigetfile([ANALYSIS_FOLDER filesep 'analysisTrees' filesep '*.mat'], 'Load analysisTree');
 
 load(fullfile(pathname, fname)); %loads analysisTree
-global analysisTree
 T = analysisTree;
 %analysisClass = 'DriftingGratingsAnalysis';
 analysisClass = 'BarsMultiAngleAnalysis';
@@ -21,25 +18,26 @@ for i=1:L
     curNode = nodes(i);
     cellName = {T.getCellName(curNode)};
     
-    if any(contains(OSICells, cellName));
-        continue
+    if any(contains(OSICells, cellName))
+        display('already got that one')
     else
     	curNode = T.subtree(curNode);
     	OSICells(Count) = cellName;
     	
-    	Switch analysisClass
-    		case analysisClass == 'DriftingGratingsAnalysis'
+    	switch analysisClass
+    		case 'DriftingGratingsAnalysis'
     			curNodeData = curNode.get(4);
     			OSI(Count) = curNodeData.F1amplitude_OSI;
         		OSAng(Count) = curNodeData.F1amplitude_OSang;
-    		case analysisClass == 'BarsMultiAngleAnalysis'
+    		case 'BarsMultiAngleAnalysis'
         		curNodeData = curNode.get(1);
-				OSI(Count) = curdNodeData.spikeCount_stimInterval_baselineSubtracted_OSI
+				OSI(Count) = curNodeData.spikeCount_stimInterval_baselineSubtracted_OSI
         		OSAng(Count) = curNodeData.spikeCount_stimInterval_baselineSubtracted_OSang
        		otherwise
-        		display('We don't recognize this analysisClass')
-       		end	
-        
+        		display('We do not recognize this analysisClass')
+        end
         Count = Count + 1;
     end
+        
+end
 end
