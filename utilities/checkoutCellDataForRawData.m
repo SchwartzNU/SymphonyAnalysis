9f461cb1acbd2f1724ddd89605a7bc5b282f426a
davidswygart <davidswygart2015@u.northwestern.edu>
2019-06-12 15:58:09 -0500

    Update checkoutCellDataForRawData.m

--- utilities/checkoutCellDataForRawData.m
+++ utilities/checkoutCellDataForRawData.m
@@ -1,26 +1,39 @@
-function [] = checkoutCellDataForRawData()
+function [] = checkoutCellDataForRawData(expname)
 global CELL_DATA_FOLDER;
 global CELL_DATA_MASTER;
 global RAW_DATA_FOLDER;
 
+%% Check connection to Server
 if ~exist(CELL_DATA_MASTER,'dir')
     error('Could not connect to CellDataMaster')
+else
+    disp('Connected to server verified')
 end
 
-DataName = uigetfile([RAW_DATA_FOLDER,'*.h5'], 'Choose raw data file');
-DataName = erase(DataName,'.h5');
+%% Choose a raw data file if no experiment day is given
+if nargin == 0
+    expname = uigetfile([RAW_DATA_FOLDER,'*.h5'], 'Choose raw data file');
+    expname = erase(expname,'.h5');
+end
 
+%% Look for cellData on server that matched the experiment day
 disp('Looking for matching cellData in CellDataMaster')
-CellData_Master = dir([CELL_DATA_MASTER,DataName,'*']);
+CellData_Master = dir([CELL_DATA_MASTER,expname,'*']);
 
 if length(CellData_Master) < 1
     disp('No matching cell data found on server')
+else
+    fprintf('%d matching files found on the server \n', length(CellData_Master))
 end
 
-CellData_Local = dir([CELL_DATA_FOLDER,DataName,'*']);
+%% Check for any local cellData that is already on your computer for this experiment.
+disp('Checking your local Cell Data Folder')
+CellData_Local = dir([CELL_DATA_FOLDER,expname,'*']);
 CellData_LocalNames = struct2cell(CellData_Local);
 CellData_LocalNames = CellData_LocalNames(1,:);
+fprintf('%d matching files found locally \n', length(CellData_LocalNames))
 
+%% Copy over cellData from the server that matched the experiment and is not already in your local folder.
 for i = 1:length(CellData_Master)
     if any(strcmp(CellData_LocalNames, CellData_Master(i).name))
         fprintf('%s is already in your local cellData \n', CellData_Master(i).name)
