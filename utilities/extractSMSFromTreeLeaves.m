6a88765f1340edd64dd40ca8119c54dc4eecf6c5
Sam Cooler <scooler@gmail.com>
2019-08-20 15:43:20 -0500

    fixes for sms tool

--- utilities/extractSMSFromTreeLeaves.m
+++ utilities/extractSMSFromTreeLeaves.m
@@ -1,9 +1,16 @@
-function [] = extractSMSFromTreeLeaves(T, fname)
-global CELL_DATA_FOLDER
+% function [] = extractSMSFromTreeLeaves(T)
+% global CELL_DATA_FOLDER
 %T is analysis tree
 
 leafNodes = T.findleaves;
 nLeaves = length(leafNodes);
+
+
+cellNames = {};
+
+baseline = linspace(0, 1200, 200);
+all_ON = [];
+all_OFF = [];
 
 lastNodeID = 0;
 for i=1:nLeaves    
@@ -12,20 +19,39 @@ for i=1:nLeaves
         
         
         if curNodeID ~= lastNodeID
-            [cellName, rest] = strtok(curNode.name, ':');
-            datasetName = strtok(deblank(rest), ':');
-            datasetName = datasetName(2:end);
+                [cellName, rest] = strtok(curNode.name, ':');
+                if i > 1 && strcmp(cellName,cellNames{end,1})
+                    continue
+                end
+                cellNames{end+1,1} = cellName;
+                datasetName = strtok(deblank(rest), ':');
+                datasetName = datasetName(2:end);
 
-            %[psth_y, psth_x] = cellData.getPSTH(epochInd);
-            [sms_x] = curNode.spotSize;
-            [sms_y_on]  = curNode.spikeCount_stimInterval.mean_c;
-            [sms_y_off] = curNode.spikeCount_afterStim.mean_c;
-            s = struct;
-            s.sms_x = sms_x;
-            s.sms_y_on = sms_y_on;
-            s.sms_y_off = sms_y_off;
-            igorName = [cellName '_' datasetName];
-            exportStructToHDF5(s, fname, igorName);
+%                 curNode
+                %[psth_y, psth_x] = cellData.getPSTH(epochInd);
+                try
+                    [sms_x] = curNode.spotSize;
+                catch
+                    continue
+                end
+                   
+                [sms_y_on]  = curNode.spikeCount_stimInterval.mean_c;
+                [sms_y_off] = curNode.spikeCount_afterStim.mean_c;
+
+                y_on = interp1(sms_x, sms_y_on, baseline);
+                y_off = interp1(sms_x, sms_y_off, baseline);
+
+
+                all_ON(end+1, :) = y_on;
+                all_OFF(end+1, :) = y_off;
+    %             s = struct;
+    %             s.sms_x = sms_x;
+    %             s.sms_y_on = sms_y_on;
+    %             s.sms_y_off = sms_y_off;
+    %             igorName = [cellName '_' datasetName];
+    %             exportStructToHDF5(s, fname, igorName);
         end
         lastNodeID = curNodeID;
 end
+
+
