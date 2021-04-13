c951aa07ecd7a2f4b91c6de8bf2131ceafa396d2
davidswygart <davidswygart2015@u.northwestern.edu>
2021-04-13 00:38:11 -0500

    Update initializeExperiment.m
    included case for cellData on Server but not on local.

--- utilities/initializeExperiment.m
+++ utilities/initializeExperiment.m
@@ -4,14 +4,23 @@ function initializeExperiment(expname, myname)
     status = checkoutCellDataForRawData(expname);
     switch status
         case -1
-            a = input(['No server connection could be made. Only proceed if you',...
-                '\nknow that the data has not been previously parsed.',...
-                '\nAre you sure you want to proceed? [y/n] '], 's');
+            warning('No server connection could be made. Only proceed if you know that the data has not been previously parsed.')
+            a = input('Are you sure you want to proceed? [y/n] ', 's');
             if a ~= 'y'
                 return
             else
                 parseRawDataFiles(expname);
             end
+            
+        case 1
+            warning('cellData for this experiment is already on the server. Parsing will overwrite the cellData on the server.')
+            a = input('Are you sure you want to parse new cellData? [y/n] ', 's');
+            if a ~= 'y'
+                return
+            else
+                parseRawDataFiles(expname);
+            end
+            
         case 0
             parseRawDataFiles(expname);
     end
