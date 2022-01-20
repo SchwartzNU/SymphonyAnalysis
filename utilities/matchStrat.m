a36c399ac77bde9f502be0adf1f576ece73c43dd
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-01-19 20:24:20 -0600

    updates for morphology analysis

--- utilities/matchStrat.m
+++ utilities/matchStrat.m
@@ -14,13 +14,20 @@ shiftAllowance_steps = round(shiftAllowance ./ x_step);
 for i=1:N_templates
     bestFit = 0;
     bestShift_cur = 0;
-    sum_sqared_data = sum((stratY_interp - mean(stratY_interp)).^2);
+    %sum_sqared_data = sum((stratY_interp - mean(stratY_interp)).^2);
     
     for j=-shiftAllowance_steps:shiftAllowance_steps
         data_shifted = circshift(stratY_interp, j);
+        %keyboard;
+        %R^2 way
+        %fitVal = 1 - sum((data_shifted - templates.strat_mean{i}).^2) / sum_sqared_data;   
         
-        fitVal = 1 - sum((data_shifted - templates.strat_mean{i}).^2) / sum_sqared_data;       
+        %cosine similarity
+        fitVal = (data_shifted' * templates.strat_mean{i}) / ...
+            (sqrt(sum(data_shifted.^2)) *  sqrt(sum(templates.strat_mean{i}.^2)));
         
+        %pause;
+                
         if fitVal > bestFit
             bestFit = fitVal;
             bestShift_cur = j;
