1529edf5f950a920eb9ef90cd7bf83efe4c6faa3
SchwartzLabNU <SchwartzLabNU@gmail.com>
2022-07-28 14:06:35 -0500

    Create MEIAnalysis.m

---
+++ analysisTreeClasses/MEIAnalysis.m
@@ -0,0 +1,17 @@
+classdef MEIAnalysis < AnalysisTree
+
+methods
+    function obj = MEIAnalysis(cellData, dataSetName, params)
+    end
+
+    function obj = doAnalysis(obj, cellData)
+%         rootData = obj.get(1);
+%         leafIDs = obj.findleaves();
+%         L = length(leafIDs);
+%         baseline = zeros(1,L);
+%         
+        
+    end
+end
+
+end
