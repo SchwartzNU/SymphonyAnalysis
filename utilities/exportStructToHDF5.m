b323b03ceff65d75a9575f99d3dc8178c84b69ab
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-15 18:58:01 -0600

    Update exportStructToHDF5.m

--- utilities/exportStructToHDF5.m
+++ utilities/exportStructToHDF5.m
@@ -9,49 +9,48 @@ function exportStructToHDF5(s, fileName, dataRoot)
 % dataRoot: hdf5 root to put s under.
 
 fileExists = exist(fileName, 'file');
-% 
+%
 if ~fileExists
     fid = fopen(fileName, 'w');
     fclose(fid);
 end
-    
+
 fNames = fieldnames(s);
 
 for i = 1:length(fNames)
-    if isstruct(s.(fNames{i}))        
-        s_part = s.(fNames{i});
-        if isfield(s_part, 'type') %special handling for nodeData structs
-            if strcmp(s_part.type, 'byEpoch')
-                name = [fNames{i} '_mean_c'];
-                val = s_part.mean_c;
-                hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
-                name = [fNames{i} '_median_c'];
-                val = s_part.median_c;
-                hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
-                name = [fNames{i} '_SD_c'];
-                val = s_part.SD_c;
-                hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
-                name = [fNames{i} '_SEM_c'];
-                val = s_part.SEM_c;
-                hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
-            elseif strcmp(s_part.type, 'singleValue')
-                name = fNames{i};
-                val = s_part.value;
-                hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
-            end
-        else %struct handling: make subfolder
-            struct_name = fNames{i};
-            subStruct_fnames = fieldnames(s_part);
-            for j=1:length(subStruct_fnames)
-                hdf5write(fileName, strcat(dataRoot, '/', fNames{i}, '/', subStruct_fnames{j}), s_part.(subStruct_fnames{j}), 'WriteMode', 'append');
-            end            
-        end
-        
-    else        
+    if isstruct(s.(fNames{i}))
+        exportStructToHDF5(s.(fNames{i}), fileName, strcat(dataRoot, '/', fNames{i}));
+    else
         if ~isempty(s.(fNames{i}))
             if ~strcmp(class(s.(fNames{i})), 'cfit')
-                hdf5write(fileName, strcat(dataRoot, '/', fNames{i}), s.(fNames{i}), 'WriteMode', 'append');
+                try
+                    hdf5write(fileName, strcat(dataRoot, '/', fNames{i}), s.(fNames{i}), 'WriteMode', 'append');
+                catch
+                    disp('Did not overwrite existing dataset');
+                end
             end
         end
     end
 end
+
+%
+% if isfield(s_part, 'type') %special handling for nodeData structs
+%             if strcmp(s_part.type, 'byEpoch')
+%                 name = [fNames{i} '_mean_c'];
+%                 val = s_part.mean_c;
+%                 hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
+%                 name = [fNames{i} '_median_c'];
+%                 val = s_part.median_c;
+%                 hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
+%                 name = [fNames{i} '_SD_c'];
+%                 val = s_part.SD_c;
+%                 hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
+%                 name = [fNames{i} '_SEM_c'];
+%                 val = s_part.SEM_c;
+%                 hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
+%             elseif strcmp(s_part.type, 'singleValue')
+%                 name = fNames{i};
+%                 val = s_part.value;
+%                 hdf5write(fileName, strcat(dataRoot, '/', name), val, 'WriteMode', 'append');
+%             end
+
